3ed85de8dec190a4f43b745edc9ae71381a8375f
Ana Luiza <analuipiresbar@gmail.com>
2021-08-29 22:59:07 -0300

    Alteracao da URL de prod

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -1,4 +1,8 @@
 import axios from 'axios'
 
-const getFromAPI = axios.create({baseURL: "http://localhost:3333/" })
+// DEV
+// const getFromAPI = axios.create({baseURL: "http://localhost:3333/" })
+
+// PROD
+const getFromAPI = axios.create({baseURL: "https://banco-de-curriculos-back.herokuapp.com/" })
 export default getFromAPI
